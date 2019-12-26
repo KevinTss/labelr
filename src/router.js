7377a1e3aef1eb2e370905827aed99b670490b72
Kevin Tassi <kevin@wooclap.com>
2019-12-26 16:34:47 +0100

    Add Layout component

--- src/router.js
+++ src/router.js
@@ -1,19 +1,27 @@
 import Router from "ampersand-router";
 import React from "react";
 import ReactDOM from "react-dom";
+import Layout from "./components/Layout";
 import HomePage from "./pages/home";
 import RepoPage from "./pages/repo";
 
 export default Router.extend({
+  renderPage(page, options = { layout: true }) {
+    if (options.layout) {
+      page = <Layout>{page}</Layout>;
+    }
+    ReactDOM.render(page, document.querySelector("#app"));
+  },
+
   routes: {
     "": "home",
     repos: "repos"
   },
 
   home() {
-    ReactDOM.render(<HomePage />, document.querySelector("#app"));
+    this.renderPage(<HomePage />, { layout: false });
   },
   repos() {
-    ReactDOM.render(<RepoPage />, document.querySelector("#app"));
+    this.renderPage(<RepoPage />);
   }
 });
