dd4964b1eb672f383bcd6035f359382312120616
Kevin Tassi <kevin@wooclap.com>
2019-12-26 12:33:01 +0100

    Config webpack 4 working

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+
+  entry: "./src/app.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8888
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            attrs: [":data-src"]
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "src/template.html"
+    })
+  ]
+};
